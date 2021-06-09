415bd4b2773a0438c71bfd5c00e2bc4456420c63
SunnydayXD <911219730@qq.com>
2021-06-09 22:38:19 +0800

    2021/06/09 22:35 home.vue router/index.js

--- addinf/src/router/index.js
+++ addinf/src/router/index.js
@@ -2,19 +2,29 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Addinf from '../views/Addinf.vue'
 import Yimiao from '../views/Yimiao.vue'
-
+import { Button } from 'mint-ui';
+import { Popup } from 'vant';   //弹出框
 
 Vue.use(VueRouter)
+Vue.use(Vuex);
+Vue.component(Button.name, Button);
+Vue.use(Popup); //弹出框
 
 const routes = [
   {
     path: '/',
     name: 'Addinf',
     component: Addinf
-  },{
+  },
+  {
     path: '/yimiao',
     name: 'Yimiao',
     component: Yimiao
+  },
+  {
+      path: '/home',
+      name: 'Home',
+      component: Home
   }
 ]
 
