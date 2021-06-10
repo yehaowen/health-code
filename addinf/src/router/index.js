5b0497783972e79b79aa4a28010ee1f0c4e99c85
chensulun <chensulun@126.com>
2021-06-10 20:35:55 +0800

    6/10 20:35 修正 陈苏伦

---
+++ addinf/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Addinf from '../views/Addinf.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Addinf',
+    component: Addinf
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
