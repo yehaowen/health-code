12b4cdec3d99f98230a3a41c3580e7cb574bc386
chensulun <chensulun@126.com>
2021-06-10 20:56:09 +0800

    6/10 20:56 修正 陈苏伦

--- healthcode/src/main.js
+++ healthcode/src/main.js
@@ -2,8 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.min.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { Checkbox, CheckboxGroup } from 'vant';
+import 'vant/lib/index.css';
 
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(ElementUI);
+Vue.use(MintUI)
 Vue.config.productionTip = false
+
+// 配置axios
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3000/';
+Vue.prototype.axios = axios;
+
+// 配置Moment
+import moment from 'moment';
+Vue.prototype.moment = moment;
 
 new Vue({
   router,
