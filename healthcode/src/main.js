dc76a224cca88b67041b60daa0e6b7ac804c729d
chensulun <chensulun@126.com>
2021-06-11 11:52:58 +0800

    6/11 11:52 在 mypart.vue中添加二维码模块 陈苏伦

--- healthcode/src/main.js
+++ healthcode/src/main.js
@@ -2,21 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.min.css'
 import 'mint-ui/lib/style.css'
+
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
 import { Checkbox, CheckboxGroup } from 'vant';
 import { Popup } from 'vant';   //弹出框
 import 'vant/lib/index.css';
-
-Vue.use(Checkbox);
-Vue.use(CheckboxGroup);
-Vue.use(ElementUI);
-Vue.use(MintUI);
-Vue.use(Popup); //弹出框
-Vue.config.productionTip = false
 
 // 配置axios
 import axios from 'axios';
@@ -26,6 +22,13 @@ Vue.prototype.axios = axios;
 // 配置Moment
 import moment from 'moment';
 Vue.prototype.moment = moment;
+
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(ElementUI);
+Vue.use(MintUI);
+Vue.use(Popup); //弹出框
+Vue.config.productionTip = false
 
 new Vue({
   router,
