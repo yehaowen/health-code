daba8467c3df260758471f63d807b4476ed4a2e8
SunnydayXD <81358534+SunnydayXD@users.noreply.github.com>
2021-06-15 21:19:35 +0800

    Update index.js
    1

--- healthcode/src/store/index.js
+++ healthcode/src/store/index.js
@@ -5,8 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    hname : '',
+    hID : ''
   },
   mutations: {
+    updateUserInfo(state,userinfo){
+      state.hname = userinfo.hname;
+      state.hID = userinfo.hID;
+      // console.log(state.hname,state.hID);
+    }
   },
   actions: {
   },
